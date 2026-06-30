55f53f1341e6308bda8094caf88dfdb8a9c76f3c
duriskifeb <rizkydurfeb976@gmail.com>
2026-07-01 01:59:24 +0700

    'Deploy

---
+++ _next/static/rgdPoLOE-68_J24QI1Cu3/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-ec8cb7141665ebf7.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
